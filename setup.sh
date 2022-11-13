18ea42b9d1595b2889346d789ffc03c0a9bbe353
FredericWantiez <frederic.wantiez@gmail.com>
2022-11-13 16:51:25 +0000

    Submodules

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 set -eu
+
+git submodule init
+git submodule update
 
 # Install zsh
 sudo apt update
@@ -13,3 +15,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Install nvim
 curl -LO https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
 sudo apt -y install ./nvim-linux64.deb
+
+# Packer for nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
